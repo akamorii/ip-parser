d512637ed1eab59452fdffed67137ca33d5a6ab7
akamorii <147515917+akamorii@users.noreply.github.com>
2024-08-16 15:53:36 +0300

    update ip unpack

---
+++ unpack_ip.py
@@ -0,0 +1,11 @@
+
+def unpack_ip(ip):
+    def unpack(data):
+        if type(data) == list:
+            data = data[0]
+        return data
+    res = [unpack(n) for n in arr]
+    string = ''
+    for char in res:
+        string += str(char)
+    return string
